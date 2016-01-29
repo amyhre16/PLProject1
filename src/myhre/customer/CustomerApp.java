aeec867057201407e77a284edabcb5703e1f7dbf
Anthony Myhre <anthony.myhre@utexas.edu>
2016-01-28 18:33:13 -0600

    Moved things around

---
+++ src/myhre/customer/CustomerApp.java
@@ -0,0 +1,11 @@
+package cannata;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+        System.out.println("Hello again");
+    }
+
+}
